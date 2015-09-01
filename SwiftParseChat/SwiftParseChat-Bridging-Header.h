fda0e6499e73bbd8a55f4aa5f10d1b2e3deed2e8
psri <mail@psrikarthik.me>
2015-09-01 09:56:53 +0100

    First rev

---
+++ SwiftParseChat/SwiftParseChat-Bridging-Header.h
@@ -0,0 +1,8 @@
+
+
+#import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
+#import <ParseFacebookUtils/PFFacebookUtils.h>
+#import <ProgressHUD/ProgressHUD.h>
+#import <JSQMessages.h>
+#import <APAddressBook-Bridging.h>
